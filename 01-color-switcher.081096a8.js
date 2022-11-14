b5be4e4182783a562670ac7840684876b4752dad
777Afanas <serhii2111@yahoo.com>
2022-11-14 23:11:43 +0000

    Deploying to gh-pages from @ 777Afanas/goit-js-hw-09@3636096a0e3e5998c82400939567f6eb2bc80c88 🚀

---
+++ 01-color-switcher.081096a8.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[dats-stop]");
+//# sourceMappingURL=01-color-switcher.081096a8.js.map
---
+++ 01-color-switcher.8b8b1126.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[dats-stop]");
+//# sourceMappingURL=01-color-switcher.8b8b1126.js.map
