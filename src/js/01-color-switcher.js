e1ff9e37693e2ca1892686729e290fa9fb63cc90
Serhii Yatsenko <serhii2111@yahoo.com>
2022-11-17 06:47:20 +0200

    start task one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,34 @@
 
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[dats-stop]');
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+// const backgroundColor = document.querySelector('body');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+// startBtn.addEventListener('click', () => {
+//     timerId = setInterval(() => {
+//         backgroundColor.style.background = `${getRandomHexColor()}`;
+//     }, 1000);
+//     startBtn.disabled = true;
+//     stopBtn.disabled = false;
+// });
+
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+
+startBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+});
