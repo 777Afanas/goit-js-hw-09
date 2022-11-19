63eadd8d89cb4d24aa3246ffcf2ee1540f43b879
777Afanas <serhii2111@yahoo.com>
2022-11-19 21:44:48 +0000

    Deploying to gh-pages from @ 777Afanas/goit-js-hw-09@13040ef502fbf07e15f3b523c6c7a71282f5ae21 🚀

--- 03-promises.d74321cd.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),t={};function n(e,t){return new Promise(((n,o)=>{const s=Math.random()>.3;setTimeout((()=>{s?n(`✅ Fulfilled promise ${e} in ${t}ms`):o(`❌ Rejected promise ${e} in ${t}ms`)}),t)}))}function o(e){console.log(e)}function s(e){console.log(e)}e.addEventListener("input",(e=>{const{name:n,value:o}=e.target;t[n]=o}));e.addEventListener("submit",(r=>{r.preventDefault();for(let e=0,r=+t.delay;e<t.amount;e+=1,r+=+t.step){n(e+1,r).then(o).catch(s)}e.reset()}));
-//# sourceMappingURL=03-promises.d74321cd.js.map
